7063f8da50509d55323abd4784a8d099ac76f989
Santiago Cortes <s.cortes@uniandes.edu.co>
2020-03-30 19:19:57 +0100

    Initial structure of the package and first lines of code (Class definition of a Perceptron, as well as its constructor)

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read( )
+
+setuptools.setup(
+    name="s-cortes", # Replace with your own username
+    version="0.0.1",
+    author="Santiago Cortes Fernandez",
+    author_email="santiago.cortes0{at}gmail.com",
+    description="Simple Rosenblatt's Perceptron implementation for distinguishing a pair of linear of separable patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/s-cortes/perceptron",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ solver/__init__.py
@@ -0,0 +1,1 @@
+from simple_perceptron import SimplePerceptron
